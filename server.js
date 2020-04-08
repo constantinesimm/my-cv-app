f313fb6ebd80bd7065385ede8246b8ee2da3133e
Constantine Simm <constantine.peschanov@gmail.com>
2020-04-08 23:08:40 +0300

    added MongoDB connection for dev and prod

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+/**
+ * if app environment mode is development - get env variables
+ */
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+/**
+ * define app dependencies
+ */
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -9,11 +15,16 @@ const { mongoDatabase } = require('./server/service');
 
 const server = express();
 
-//connect database
+/**
+ * connect MongoDB
+ */
 mongoDatabase()
     .then( () => console.log('MongoDB connected'))
     .catch(error => console.log(`MongoDB connection error with message: "${ error }"`));
 
+/**
+ * app global middleware
+ */
 server.use(logger('dev'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
