da458e9cf43ca10595c617974d4f5569f38012e2
Constantine Simm <constantine.peschanov@gmail.com>
2020-04-08 23:38:10 +0300

    add app error handler + middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { mongoDatabase } = require('./server/service');
+const { HttpError } = require('./server/middleware');
 
 const server = express();
 
@@ -30,5 +31,24 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(cookieParser());
 server.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * app controller routes
+ */
+
+/**
+ * catch 404 error and send to error handler fn
+ */
+server.use((req, res, next) => next(new HttpError(404, `Not Found ${req.path}`)));
+
+/**
+ * app central error handler
+ */
+server.use((error, req, res, next) => {
+    if (error.status) return res.status(error.status).json({ message: error.message });
+    if (error.errors) return res.status(400).json({ error: { name: error.name, errors: error.errors } });
+
+    next(error);
+});
 
 module.exports = server;
